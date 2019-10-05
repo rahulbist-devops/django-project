3b1bed32c6511c115c7c6316cc2a55647e3d7096
rahulbist-devops <rbist@equalexperts.com>
2019-10-05 20:49:46 +0530

    files

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns the list of API features list"""
+        an_apiview = [
+            'http methods:get, put, post, patch, delete',
+            'You can aadd anything here'
+        ]
+
+        return Response({'message':'Hello','an_apiview':an_apiview})
+
