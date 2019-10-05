33f54415af973039500fb75cad012e505b1eab6e
rahulbist-devops <rbist@equalexperts.com>
2019-10-05 21:35:23 +0530

    files

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from myapp import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
-
+    serializer_class = serializers.HelloSerializer 
     def get(self, request, format=None):
         """Returns the list of API features list"""
         an_apiview = [
@@ -13,4 +16,33 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello','an_apiview':an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """ Handle update to the objects"""
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        """ Handle partial updates to the objects"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """ Handle deleting of the objects"""
+        return Response({'method': 'DELETE '})
+
+
+
 
